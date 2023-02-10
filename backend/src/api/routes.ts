6b998a291a75d647a51a9462040794fed76a8588
Filipe Onoue <filipeflom@gmail.com>
2023-02-10 16:29:46 -0300

    feat: :sparkles: implemented routes.ts

---
+++ backend/src/api/routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { userRoutes } from '../models/User/userRoutes';
+import { transactionRoutes } from '../models/Transaction/trensactionRoutes';
+
+export const routes = express.Router();
+
+routes.use(userRoutes);
+routes.use(transactionRoutes);
