77f23dae96e819e011965b7c2d4cb6f4ec231a32
Filipe Onoue <filipeflom@gmail.com>
2023-02-13 16:23:54 -0300

    feat: :sparkles: iniciate implement from Register and Wallet pages

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,7 @@
+import { MantineProvider, Text } from '@mantine/core';
+
+export default function Register() {
+  return (
+    <Text>Register</Text>
+  );
+}
---
+++ frontend/src/pages/Wallet.tsx
@@ -0,0 +1,7 @@
+import { MantineProvider, Text } from '@mantine/core';
+
+export default function Wallet() {
+  return (
+      <Text>Wallet</Text>
+  );
+}
