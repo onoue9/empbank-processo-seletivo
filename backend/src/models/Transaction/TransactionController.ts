e3484c3f2472ba5486c6e3b44e92b8d5c6934ff1
Filipe Onoue <filipeflom@gmail.com>
2023-02-13 16:18:21 -0300

    feat: :sparkles: implemented a new method do getAllTransactionByUserID

--- backend/src/models/Transaction/TransactionController.ts
+++ backend/src/models/Transaction/TransactionController.ts
@@ -9,6 +9,16 @@ export default class TransactionController {
     this.TransactionService = new TransactionService();
   }
 
+  async getAllTransactionsByUserId(req: Request, res: Response): Promise<Response> {
+    const { userId } = req.params;
+    try {
+      const transactions = await this.TransactionService.getAllTransactionsByUserId(parseFloat(userId));
+      return res.status(200).json(transactions);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+
   async createTransaction(req: Request, res: Response): Promise<Response> {
     const { userId, title, value, type, category } = req.body;
     try {
