a00a69d52583a0440e91aa629a93abb8bdc4f7aa
Filipe Onoue <filipeflom@gmail.com>
2023-02-13 16:20:52 -0300

    refactor: :recycle: refact params type to number

--- backend/src/models/User/UserService.ts
+++ backend/src/models/User/UserService.ts
@@ -11,24 +11,24 @@ export default class UserService {
 
   async getAllUsers(): Promise<UserInterface[]> {
     const allUsers = await this.UserModel.getAllUsers();
-
+    
     if (!allUsers) {
       throw new Error("No users found");
     }
     
     return allUsers;
   }
-
-  async getUser(user: UserInterface): Promise<UserInterface> {
-    const foundUser = await this.UserModel.getUserById(user);
-
+  
+  async getUser(userId: number): Promise<UserInterface> {
+    const foundUser = await this.UserModel.getUserById(userId);
+    
     if (!foundUser) {
       throw new Error("User not found");
     }
-
+    
     return foundUser;
   }
-
+  
   async loginUser(user: UserInterface) {
     const foundUser = await this.UserModel.loginUser(user);
 
@@ -38,17 +38,17 @@ export default class UserService {
 
     return foundUser;
   }
-
+  
   async registerUser(user: UserInterface) {
     const emailRegex = /\S+@\S+\.\S+/;
     const { email } = user;
-
+    
     if (!emailRegex.test(email)) {
       throw new Error("Invalid email");
     }
-
+    
     const createUser = await this.UserModel.createUser(user);
-
+    
     if (!createUser) {
       throw new Error("User not created");
     }
