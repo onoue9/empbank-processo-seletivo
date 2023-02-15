f50f6eee4f35d08b3a77b34512b2009748eba0f0
Filipe Onoue <filipeflom@gmail.com>
2023-02-15 18:02:04 -0300

    feat: :sparkles: implement a middleware validate on login and register

--- backend/src/models/User/userRoutes.ts
+++ backend/src/models/User/userRoutes.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { userController } from './index';
+import { userRegisterValidation } from '../../middlewares/UserValidation';
+import { loginValidation } from '../../middlewares/UserValidation';
 
 export const userRoutes = express.Router();
 
 userRoutes.get('/', (req, res) => userController.getAllUsers(req, res));
 userRoutes.get('/:userId', (req, res) => userController.getUser(req, res));
-userRoutes.post('/login', (req, res) => userController.loginUser(req, res));
-userRoutes.post('/register', (req, res) => userController.registerUser(req, res));
+userRoutes.post('/login',
+  (req, res, next) => loginValidation.validate(req, res, next),
+  (req, res) => userController.loginUser(req, res),
+);
+userRoutes.post('/register',
+  (req, res, next) => userRegisterValidation.validate(req, res, next),
+  (req, res) => userController.registerUser(req, res),
+);
