90973f04eabc18ec528af149ae7e40d35b056f23
Filipe Onoue <filipeflom@gmail.com>
2023-02-15 18:05:00 -0300

    feat: :sparkles: implement fetchRegister, useNavigate hook and responsive style

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,20 @@
-import { Button, Flex, Image, Text, TextInput, Title } from '@mantine/core';
+import { Button, Flex, Image, PasswordInput, Text, TextInput, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import LoginPageImage from '../assets/images/LoginPageImage.svg';
 import empbankLogo from '../assets/images/empbankLogo.svg';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+interface RegisterBodyInterface {
+  fullName: string,
+  email: string,
+  password: string,
+}
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [invalidUser, setInvalidUser] = useState('');
+
   const form = useForm({
     initialValues: {
       fullName: '',
@@ -12,11 +23,28 @@ export default function Register() {
     },
 
     validate: {
-      fullName: (value: string) => value.length > 0 ? null : 'Invalid full name',
-      email: (value: string) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      password: (value: string) => value.length >= 6 ? null : 'Invalid password',
+      fullName: (value: string) => value.length > 0 ? null : 'Você precisa preencher seu nome completo',
+      email: (value: string) => (/^\S+@\S+$/.test(value) ? null : 'Use um email válido'),
+      password: (value: string) => value.length >= 6 ? null : 'Sua senha deve conter mais que 5 caracteres',
     },
   })
+
+  const fetchRegister = async (body: RegisterBodyInterface) => {
+    try {
+      const response = await fetch('http://localhost:3001/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      if (data.message) return setInvalidUser(data.message);
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
   return (
       <Flex
@@ -29,6 +57,11 @@ export default function Register() {
           src={LoginPageImage}
           alt="casal vendo alguma coisa em um tablet"
           style={{ width: '60%', height: '100%', padding: '20px 0px 20px 0px' }}
+          sx={() => ({
+            '@media (max-width: 600px)': {
+              display: 'none',
+            }
+          })}
         />
         <Flex
           direction="column"
@@ -36,6 +69,13 @@ export default function Register() {
           justify="center"
           gap="xs"
           style={{ width: '35%', padding: '5% 0px 0px 0px' }}
+          sx={() => ({
+            '@media (max-width: 600px)': {
+              display: 'flex',
+              width: '80% !important',
+              height: '100%',
+            }
+          })}
         >
           <Image
             src={empbankLogo}
@@ -48,7 +88,7 @@ export default function Register() {
             order={2}
           >Faça seu cadastro</Title>
           <form 
-            onSubmit={form.onSubmit((values) => console.log(values))}
+            onSubmit={form.onSubmit((values) => fetchRegister(values))}
             style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', height: '324px' }}
           >
             <TextInput
@@ -65,14 +105,14 @@ export default function Register() {
               style={{ width: '100%', height: '100px' }}
               radius="md"
             />
-            <TextInput
+            <PasswordInput
               label="Senha"
               placeholder="Insira sua senha"
               {...form.getInputProps('password')}
-              type="password"
               style={{ width: '100%', height: '100px' }}
               radius="md"
             />
+            { invalidUser.length > 0 && <Text color='red'>{invalidUser}</Text>}
             <Button style={{ backgroundColor: '#60CFFA' }} fullWidth radius="md" size="lg" type="submit">
               <Text
                 style={{ fontWeight: '600', fontSize: '20px', lineHeight: '155%', color: '#F2F2F2' }}
@@ -84,6 +124,7 @@ export default function Register() {
             fullWidth
             radius="md"
             size='lg'
+            onClick={() => navigate('/login') }
           >
             <Text
               style={{ fontWeight: '600', fontSize: '20px', lineHeight: '155%', color: '#F2F2F2' }}
